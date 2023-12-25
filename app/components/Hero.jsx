47b29832e37e06912fcd90ae0de4fc388631f015
Tom Caron <tcaron146@gmail.com>
2023-12-25 14:30:29 -0500

    form is working email lost is a go

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,20 +1,34 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
-import Form from './Form'
 import sea from "../../public/sea-7484743_1920.jpg"
+import { useRef, useState } from 'react';
+import MailchimpSubscribe from 'react-mailchimp-subscribe';
 
 const Hero = () => {
 
+  const url = process.env.NEXT_PUBLIC_MAILCHIMP_URL;
+
+
+  const SimpleForm = () => <MailchimpSubscribe url={url}/>
+  
   return (
     <div className="w-full h-[100vh]">
-      <Image src={sea} className='w-full h-full object-cover' alt="background image"/>
+      <Image src={sea} className='w-full h-full object-cover' alt="background image" />
       <div className='m-auto'>
-        <div className='absolute px-6 py-4 top-[40%] w-full sm:-[50%] h-full'>
-          <form className='max-w-[600px] m-auto'>
-            <h1 className='text-2xl font-bold text-center'>Subscribe to the Newsletter</h1>
-            <input className='border shadow-lg p-3 w-full my-2' type="text" placeholder='Email' />
-            <button className='border shadow-lg p-3 w-full mt-2'>Submit</button>
-          </form>
+        <div className='absolute px-6 py-4 top-[70%] w-full sm:-[50%] h-full'>
+          <div className='p-3 border border-primary/0 bg-primary/20 rounded-md flex flex-col'>
+            <h1 className='text-2xl font-bold text-center text-secondary'>Subscribe to the Newsletter</h1>
+          <MailchimpSubscribe className="flex flex-col" url={process.env.NEXT_PUBLIC_MAILCHIMP_URL} 
+          render={({ subscribe, status, message }) => (
+            <div>
+              <SimpleForm onSubmitted={formData => subscribe(formData)} />
+              {status === "sending" && <div className='text-primary'>sending...</div>}
+              {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
+              {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+            </div>
+          )}/>
+          </div>
         </div>
       </div>
     </div>
